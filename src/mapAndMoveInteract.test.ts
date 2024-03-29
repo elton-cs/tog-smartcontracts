1c00bbbd4a3be7852957c852db9d441563b15f02
zkelton <govada.elton@gmail.com>
2024-03-28 23:34:12 -0500

    Start map and move contract integration testing

---
+++ src/mapAndMoveInteract.test.ts
@@ -0,0 +1,54 @@
+import { Field, Mina, PrivateKey, PublicKey, AccountUpdate, Poseidon, SmartContract, UInt64 } from 'o1js';
+import { FullMovement } from './mechanics/player/fullmovement';
+import { Position2D } from './mechanics/components';
+import { GameMap } from './mechanics/map/map';
+
+
+let proofsEnabled = true;
+
+describe('Player Position', () => {
+    let togKey: PrivateKey,
+        togAddress: PublicKey,
+        playerKey: PrivateKey,
+        playerAddress: PublicKey,
+        mapContractKey: PrivateKey,
+        mapContractAddress: PublicKey,
+        mapZkApp: GameMap,
+        fullmoveContractKey: PrivateKey,
+        fullmoveContractAddress: PublicKey,
+        fullmoveZkApp: FullMovement;
+
+    const Local = Mina.LocalBlockchain({ proofsEnabled });
+    Mina.setActiveInstance(Local);
+    ({ privateKey: togKey, publicKey: togAddress } = Local.testAccounts[0]);
+    ({ privateKey: playerKey, publicKey: playerAddress } = Local.testAccounts[1]);
+
+    beforeAll(async () => {
+        if (proofsEnabled) {
+            await GameMap.compile();
+            await FullMovement.compile();
+        }
+    });
+
+    beforeEach(() => {
+        mapContractKey = PrivateKey.random();
+        mapContractAddress = mapContractKey.toPublicKey();
+        mapZkApp = new GameMap(mapContractAddress); 
+
+        fullmoveContractKey = PrivateKey.random();
+        fullmoveContractAddress = fullmoveContractKey.toPublicKey();
+        fullmoveZkApp = new FullMovement(fullmoveContractAddress); 
+    });
+
+    async function deployContract(deployerKey:PrivateKey, contractKey: PrivateKey, zkApp: SmartContract ) {
+        let deployerPubKey = deployerKey.toPublicKey();
+        const txn = await Mina.transaction(deployerPubKey, () => {
+            AccountUpdate.fundNewAccount(deployerPubKey);
+            zkApp.deploy();
+        });
+        await txn.prove();
+        await txn.sign([deployerKey, contractKey]).send();
+    }
+
+    // unit tests
+});
