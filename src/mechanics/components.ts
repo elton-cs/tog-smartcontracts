c105725fd04457fefc3e4d6e78029b1ef0d162f8
zkelton <govada.elton@gmail.com>
2024-03-28 21:37:22 -0500

    Add new() function to instantiate new position structs

--- src/mechanics/components.ts
+++ src/mechanics/components.ts
@@ -1,3 +1,7 @@
 import { Field, Struct } from "o1js";
 
-export class Position2D extends Struct({x: Field, y: Field}){}
+export class Position2D extends Struct({x: Field, y: Field}){
+    static new(x: number, y: number){
+        return {x: Field(x), y: Field(y)}
+    }
+}
