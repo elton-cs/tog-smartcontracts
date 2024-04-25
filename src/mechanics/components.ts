b2ba458546cf4a9f9e58ce0e3b39932a2f34d7d8
zkelton <govada.elton@gmail.com>
2024-04-24 22:56:39 -0500

    Fix incorrect return statement for Position2d.new() method

--- src/mechanics/components.ts
+++ src/mechanics/components.ts
@@ -2,7 +2,10 @@ import { Field, Struct } from "o1js";
 
 export class Position2D extends Struct({ x: Field, y: Field }) {
     static new(x: number, y: number) {
-        return { x: Field(x), y: Field(y) }
+        return new Position2D({
+            x: Field(x),
+            y: Field(y),
+        })
     }
 
     add(position: Position2D): Position2D {
