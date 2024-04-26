2a3e769fa55359a3e9a067a0be5f1e8cb1d5d75d
zkelton <govada.elton@gmail.com>
2024-04-25 22:56:46 -0500

    Add DirectionVector2D struct implementation

--- src/mechanics/components.ts
+++ src/mechanics/components.ts
@@ -21,6 +21,13 @@ export class Position2D extends Struct({ x: Field, y: Field }) {
             y: this.y.add(unitVector.y),
         })
     }
+
+    addDirectionVector(directionVector: DirectionVector2D): Position2D {
+        return new Position2D({
+            x: this.x.add(directionVector.x),
+            y: this.y.add(directionVector.y),
+        })
+    }
 }
 
 export class UnitVector2D extends Struct({ x: Field, y: Field }) {
@@ -52,3 +59,39 @@ export class DamageComponent extends Struct({
     attackType: Field,
     attackRange: AttackSurface
 }) { }
+
+
+export class DirectionVector2D extends Struct({ x: Field, y: Field }) {
+    static from(direction: Field) {
+        switch (direction) {
+            case Field(1):
+                return new DirectionVector2D({ x: Field(-1), y: Field(-1) });
+            case Field(2):
+                return new DirectionVector2D({ x: Field(0), y: Field(-1) });
+            case Field(3):
+                return new DirectionVector2D({ x: Field(1), y: Field(-1) });
+            case Field(4):
+                return new DirectionVector2D({ x: Field(-1), y: Field(0) });
+            case Field(5):
+                return new DirectionVector2D({ x: Field(0), y: Field(0) });
+            case Field(6):
+                return new DirectionVector2D({ x: Field(1), y: Field(0) });
+            case Field(7):
+                return new DirectionVector2D({ x: Field(-1), y: Field(1) });
+            case Field(8):
+                return new DirectionVector2D({ x: Field(0), y: Field(1) });
+            case Field(9):
+                return new DirectionVector2D({ x: Field(1), y: Field(1) });
+            default:
+                throw new Error("Invalid direction");
+        }
+    }
+
+    multiply(scalar: Field): DirectionVector2D {
+        return new DirectionVector2D({
+            x: this.x.mul(scalar),
+            y: this.y.mul(scalar),
+        })
+    }
+
+}
